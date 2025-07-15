d1b4ac706ae91dabd5c2dd4452cc998758add187
AntManDev <AntManDev@yahoo.com>
2025-07-15 14:13:37 -0700

    setup React using Vite

---
+++ cellular-automata/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import CellularAutomataGUI from './GUI';
+
+function App() {
+  return (
+    <div>
+      <h1>Cellular Automata Engine</h1>
+      <CellularAutomataGUI />
+    </div>
+  );
+}
+
+export default App;
